65dfd91d00ee55d3c9e199102fa9f8e08393f981
Yun <ytk5495@gmail.com>
2023-07-19 18:45:17 +0900

    스프링 기본 port가 8080이 맞을까요?? 현재 http://localhost:8080/hello 로 get 요청한 상태입니다.

--- frontend/src/services/hello-backend/index.js
+++ frontend/src/services/hello-backend/index.js
@@ -7,7 +7,7 @@ const useHelloBackend = () => {
   return async () => {
     const axiosResult = await axios({
       method: "get",
-      url: 'http://localhost:8080',
+      url: `${apiAddress}/hello`,
     });
     const result = axiosResult.data;
     return result;
