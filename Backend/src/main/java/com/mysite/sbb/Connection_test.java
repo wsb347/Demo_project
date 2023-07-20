378360908cf522c98365c1125720f7772656d54a
d982h8st7 <sinnarver15@gmail.com>
2023-07-20 22:44:03 +0900

    Implement testing code.

--- Backend/src/main/java/com/mysite/sbb/Connection_test.java
+++ Backend/src/main/java/com/mysite/sbb/Connection_test.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class Connection_test {
     @GetMapping(value = "/hello")
     public ResponseEntity<String> fromBackend(){
+
         return ResponseEntity.ok("Connection test succeeded");
+    }
+    @GetMapping(value = "/")
+    public ResponseEntity<String> root(){
+        return ResponseEntity.ok("Welcome to root page. <br>All Connection set RDY TO GO!");
     }
 }
