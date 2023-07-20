9b86ba752de959f61d5097181b8dcf092278d5e2
d982h8st7 <sinnarver15@gmail.com>
2023-07-21 01:48:48 +0900

    Implemented Mocking Board RESTful API in temporary.

---
+++ Backend/src/main/java/com/project/sep/back/Entity/Article.java
@@ -0,0 +1,14 @@
+package com.project.sep.back.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
