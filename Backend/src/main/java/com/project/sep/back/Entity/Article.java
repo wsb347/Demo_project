fcd3f6f0db11a1f0675ed9a31fdb6b0bf2e7c4b4
wsb347 <wsb347@naver.com>
2023-07-24 19:11:47 +0900

    article getter setter 추가

--- Backend/src/main/java/com/project/sep/back/Entity/Article.java
+++ Backend/src/main/java/com/project/sep/back/Entity/Article.java
@@ -8,4 +8,58 @@ public class Article {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String nick;
+    private String nickname;
+    private String user_pw;
+    private String title;
+    private String body;
+    private String regDate;
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getUser_pw() {
+        return user_pw;
+    }
+
+    public void setUser_pw(String user_pw) {
+        this.user_pw = user_pw;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
 }
--- Backend/src/main/java/com/project/sep/back/controller/UserController.java
+++ Backend/src/main/java/com/project/sep/back/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.project.sep.back.controller;
 
 import com.project.sep.back.Entity.User;
-import com.project.sep.back.UserService;
+import com.project.sep.back.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
